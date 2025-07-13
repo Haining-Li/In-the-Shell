e090ed0f313128391e406f55494446307711eff0
Aquazeo <zhijiechen@stu.scu.edu.cn>
2025-07-13 22:26:01 +0800

    Audio Manager

---
+++ Assets/Scripts/SettingsMenu.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SettingsMenu : MonoBehaviour
+{
+    public GameObject settingsPanel;
+    public GameObject mainMenuPanel;
+
+    public Slider bgmSlider;
+    public Slider sfxSlider;
+
+    void Start()
+    {
+        bgmSlider.value = PlayerPrefs.GetFloat("BGMVolume", 1f);
+        sfxSlider.value = PlayerPrefs.GetFloat("SFXVolume", 1f);
+
+        bgmSlider.onValueChanged.AddListener(AudioManager.Instance.SetBGMVolume);
+        sfxSlider.onValueChanged.AddListener(AudioManager.Instance.SetSFXVolume);
+    }
+
+    public void OpenSettings()
+    {
+        mainMenuPanel.SetActive(false);
+        settingsPanel.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+        mainMenuPanel.SetActive(true);
+    }
+}
+

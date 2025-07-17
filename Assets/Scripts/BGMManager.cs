659387f1531abf7b79680fc1b6e33c5f197934ed
1421293522 <1421293522@qq.com>
2025-07-17 14:58:14 +0800

    final

--- Assets/Scripts/BGMManager.cs
+++ Assets/Scripts/BGMManager.cs
@@ -1,21 +1,54 @@
 using UnityEngine;
-using UnityEngine.Audio;
+using UnityEngine.SceneManagement;
 
 public class BGMManager : MonoBehaviour
 {
     public static BGMManager Instance;
     public AudioSource bgmSource;
+    public AudioClip[] bgmClips = new AudioClip[3];
+
+    private int currentBGMIndex = 0; 
 
     void Awake()
     {
-        if (Instance == null) {
+        if (Instance == null)
+        {
             Instance = this;
             DontDestroyOnLoad(gameObject);
-        } else {
+        }
+        else
+        {
             Destroy(gameObject);
+            return;
         }
 
         LoadVolumeSettings();
+        SceneManager.sceneLoaded += OnSceneLoaded; // 注册场景加载事件
+        PlayCurrentBGM(); 
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        PlayNextBGM();
+    }
+
+    public void PlayCurrentBGM()
+    {
+        if (bgmClips.Length == 0 || currentBGMIndex >= bgmClips.Length)
+        {
+            Debug.LogWarning("BGM clips not assigned or index out of range!");
+            return;
+        }
+
+        bgmSource.clip = bgmClips[currentBGMIndex];
+        bgmSource.loop = true;
+        bgmSource.Play();
+    }
+
+    public void PlayNextBGM()
+    {
+        currentBGMIndex = (currentBGMIndex + 1) % bgmClips.Length;
+        PlayCurrentBGM();
     }
 
     public void SetBGMVolume(float volume)
@@ -30,5 +63,9 @@ public class BGMManager : MonoBehaviour
         float volume = PlayerPrefs.GetFloat("BGMVolume", 1f);
         bgmSource.volume = volume;
     }
-}
 
+    private void OnDestroy()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }
+}
